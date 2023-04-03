350c1416d2a026ce06d394003d2e2153c77ce658
Aknurka <122055794+Aknurka@users.noreply.github.com>
2023-04-03 19:12:28 +0200

    ukol-1.py

---
+++ ukol-1.py
@@ -0,0 +1,24 @@
+jmeno = "Aknur" # Nahraď TvojeJmeno vaším jménem
+email = jmeno.lower() + "@czechitas.cz"
+print("Vytvořená emailová adresa: " + email)
+
+jmeno_a_prijmeni = input("Zadejte vaše jméno a příjmení: ")
+
+# Všechna písmena velká
+print("Všechna písmena velká: " + jmeno_a_prijmeni.upper())
+
+# Všechna písmena malá
+print("Všechna písmena malá: " + jmeno_a_prijmeni.lower())
+
+# Standardní varianta - první písmeno velké, další malá
+print("Standardní varianta: " + jmeno_a_prijmeni.capitalize())
+
+# Iniciály
+jmeno, prijmeni = jmeno_a_prijmeni.split()
+print("Iniciály: " + jmeno[0].upper() + ". " + prijmeni[0].upper() + ".")
+
+# Křestní jméno zkrácené na první písmeno a příjmení, pokud je křestní jméno delší než 5 znaků. Jinak standardní varianta.
+jmeno, prijmeni = jmeno_a_prijmeni.split()
+if len(jmeno) > 5:
+    jmeno = jmeno[0].upper() + "."
+print("Křestní jméno zkrácené na první písmeno a příjmení: " + jmeno + " " + prijmeni.capitalize())
