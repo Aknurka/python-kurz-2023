fcf1c27eb27dbf5f480cb77b8db600362b6a6fc6
Aknurka <122055794+Aknurka@users.noreply.github.com>
2023-04-24 08:58:44 +0200

    ukol-3.py

---
+++ ukol-3.py
@@ -0,0 +1,17 @@
+import json
+
+# Load data from file
+with open('ukol-3.json', encoding='utf-8') as f:
+    data = json.load(f)
+
+# Create new dictionary with pass/fail information
+result = {}
+for name, score in data.items():
+    if score >= 60:
+        result[name] = "Pass"
+    else:
+        result[name] = "Fail"
+
+# Save result to file
+with open('prospech.json', 'w', encoding='utf-8') as f:
+    json.dump(result, f)
