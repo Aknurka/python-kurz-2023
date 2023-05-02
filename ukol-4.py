6051e4568fb73785d4da17be589d4e127797a1d0
Aknurka <122055794+Aknurka@users.noreply.github.com>
2023-05-02 16:59:02 +0200

    ukol-4.py

--- ukol-4.py
+++ ukol-4.py
@@ -7,10 +7,20 @@ def validate_phone_number(number):
             return True
     return False
 
+'''def calculate_price(characters):
+    if len(characters) >= 180:
+        return (len(characters) // 180 + 1) * 3
+    else:
+        return (len(characters) // 180 + 1) * 0'''
+
 def calculate_message_price(message):
-    price_per_char = 0.01667 # 3 Kč / 180 znaků
     message_length = len(message)
-    price = (message_length // 180 + 1) * 3 # cena za započaté 180 znaků
+    if message_length < 180:
+        price = 0
+    elif message_length % 180 == 0:
+        price = (message_length // 180) * 3
+    else:
+        price = (message_length // 180 + 1) * 3
     return price
 
 phone_number = input("Zadej telefonní číslo: ")
